528b9bb895ad1d06961bd9c8e149ebae6edb20cc
NadaElshafey <nada_elshafey@nweave.com>
2024-04-19 00:06:35 +0200

    Implemented MVVM Architect https://b24-698f69.bitrix24.com/company/personal/user/1/tasks/task/view/18/
    Work Done:
    =========
    - Implemented the MVVM architecture for the app's structure.
    - Created a structured package named "data" containing sub-packages for local and remote data handling.
    - Created a "local" package to manage local data storage, including databases and SharedPreferences.
    - Created a "remote" package to handle network operations.
    - Created a "model" package to contain data classes representing responses from API calls.
    - Created an api_service package to handle network requests and responses.
    - Created the repository package, to facilitate communication between the ViewModel and data sources (local or remote).

---
+++ app/src/main/java/com/example/movieapp/data/local/local_data_source/ILocalDataSource.kt
@@ -0,0 +1,4 @@
+package com.example.movieapp.data.local.local_data_source
+
+interface ILocalDataSource {
+}
---
+++ app/src/main/java/com/example/movieapp/data/remote/remote_data_source/RemoteDataSourceImp.kt
@@ -0,0 +1,4 @@
+package com.example.movieapp.data.remote.remote_data_source
+
+class RemoteDataSourceImp {
+}
