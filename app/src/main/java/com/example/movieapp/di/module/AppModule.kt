e49291281b27db1377b1b14325f649e97fdf3ca6
NadaElshafey <nada_elshafey@nweave.com>
2024-04-20 19:50:22 +0200

    Dagger Hilt Integration https://b24-698f69.bitrix24.com/company/personal/user/1/tasks/task/view/22/
    Work done:
    ========
    - Integrated Dagger Hilt into the project for dependency injection.
    - Added Dagger Hilt dependencies to the project build.gradle file.
    - Created an Application class and registered it in the AndroidManifest.xml file to initialize Dagger Hilt.
    - Created an AppModule class to provide Dagger Hilt modules for dependency injection configuration.

---
+++ app/src/main/java/com/example/movieapp/di/module/AppModule.kt
@@ -0,0 +1,11 @@
+package com.example.movieapp.di.module
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
